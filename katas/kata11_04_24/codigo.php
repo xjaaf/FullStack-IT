c645cc0e611d196f73f5749a29e6da2759377d66
JAF <xjaaf@icloud.com>
2024-05-07 13:30:53 +0200

    función interpretar creada

--- katas/kata11_04_24/codigo.php
+++ katas/kata11_04_24/codigo.php
@@ -1,60 +1,49 @@
 <?php
 function sumaDigit($numero)
 {
-
     $numero_str = (string)$numero;
-
     $longitud = strlen($numero_str);
-
     $suma = 0;
 
     for ($i = 0; $i < $longitud; $i++) {
-
         $digito = (int)$numero_str[$i];
-
         $suma += $digito;
     }
-   var_dump($suma);
     if ($suma >= 10) {
         $suma = sumaDigit($suma);
-    } else {
-        switch ($suma) {
-
-            case 0:
-                echo "El todo.";
-                break;
-            case 1:
-                echo "Individualidad.";
-                break;
-            case 2:
-                echo "Dualidad.";
-                break;
-            case 3:
-                echo "Razón.";
-                break;
-            case 4:
-                return "Estabilidad.";
-                break;
-            case 5:
-                return "Amor.";
-                break;
-            case 6:
-                return "Destrucción.";
-                break;
-            case 7:
-                return "Sabiduría.";
-                break;
-            case 8:
-                return "Poder.";
-                break;
-            case 9:
-                return "Generosidad.";
-                break;
-        }
+    }
+    return $suma;
+}
+function interpResultado($suma)
+{
+    switch ($suma) {
+        case 0:
+            return "El todo.";
+        case 1:
+            return "Individualidad.";
+        case 2:
+            return "Dualidad.";
+        case 3:
+            return "Razón.";
+        case 4:
+            return "Estabilidad.";
+        case 5:
+            return "Amor.";
+        case 6:
+            return "Destrucción.";
+        case 7:
+            return "Sabiduría.";
+        case 8:
+            return "Poder.";
+        case 9:
+            return "Generosidad.";
+        default:
+            return "Número fuera del rango válido.";
     }
 }
 
-$numero = 0;
+$numero = 53;
 
-$resultado = sumaDigit($numero);
+$suma = sumaDigit($numero);
+$resultado = interpResultado($suma);
 echo $resultado;
