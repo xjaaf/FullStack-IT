b1e6e8d6890f3d364078b2a35af0859eb25ba4c7
JAF <xjaaf@icloud.com>
2024-05-09 11:39:38 +0200

    kata print 1-10

---
+++ katas/kata09_05_24/codigo.php
@@ -0,0 +1,40 @@
+<?php 
+
+for ($i= 1; $i <=10; $i++){
+    echo $i . PHP_EOL;
+}
+
+$i = 1;
+do {
+    echo $i . PHP_EOL;
+    $i++;
+} while ($i <= 10); 
+
+$i = 1;
+while ($i <= 10){
+    echo $i . PHP_EOL;
+    $i++;
+}
+ 
+function printNumbers($start, $end){
+    for ($i = $start; $i <= $end; $i++){
+        echo $i . PHP_EOL;
+    }
+}
+
+printNumbers(1, 10);
+
+function countRecursive($num) {
+    if ($num <= 10) {
+        echo $num . PHP_EOL;
+        countRecursive($num + 1);
+    }
+}
+
+countRecursive(1);
+
+foreach (range(1, 10) as $number) {
+    echo $number . PHP_EOL;
+}
+
+?>
